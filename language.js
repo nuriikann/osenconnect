ab9c228d289b19664c81b152bf0a3a7a38316e04
nuriikann <nuri.kann@outlook.com>
2025-01-22 16:56:23 +0300

    first commit

---
+++ language.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const languageSelect = document.getElementById('language-select');
+
+    // Seçilen dili kaydet
+    languageSelect.addEventListener('change', (event) => {
+        const selectedLang = event.target.value;
+        localStorage.setItem('preferredLanguage', selectedLang);
+        window.location.href = `index-${selectedLang}.html`;
+    });
+
+    // Önceden seçilen dili kontrol et
+    const savedLanguage = localStorage.getItem('preferredLanguage');
+    if (savedLanguage && !window.location.href.includes(`index-${savedLanguage}.html`)) {
+        window.location.href = `index-${savedLanguage}.html`;
+    }
+});
